ed5ac0e939cc3f7a12c1f2ad09ccd432c7eb8328
zouhairze <zerzkhanezouhair@gmail.com>
2024-03-28 08:22:05 +0100

    j écrit le CRUD user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+class UserController extends Controller
+{
+    public function CreateUser(Request $request)
+    {
+        $UserValidate = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8',
+            'role' => 'required|string',
+        ]);
+
+        $user = new User();
+        $user->name = $UserValidate['name'];
+        $user->email = $UserValidate['email'];
+        $user->password = Hash::make($UserValidate['password']);
+        $user->role = $UserValidate['role'];
+
+        $user->save();
+
+        return response()->json('Creation avec success');
+    }
+
+    public function UpdateUser(Request $request)
+    {
+        $user = User::findOrfail($request->id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->role = $request->role;
+
+        $user->update();
+        return response()->json('Mettre à jour avec success ');
+    }
+
+    public function DeleteUser(Request $request)
+    {
+        $deleteUser = User::findOrfail($request->id)->delete();
+        return response()->json('suppression avec success');
+    }
+
+    public function GetUser()
+    {
+        $user = User::all();
+        return response()->json($user);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('CreateUser',[UserController::class,'CreateUser']);
+Route::PUT('UpdateUser',[UserController::class,'UpdateUser']);
+Route::DELETE('DeleteUser',[UserController::class,'DeleteUser']);
+Route::get('GetUser',[UserController::class,'GetUser']);
